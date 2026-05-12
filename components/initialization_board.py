52ae972890de13f305b73b59732505d15ab9e0c6
MenachemKazido <agbg7172@gmail.com>
2026-05-12 11:17:37 +0300

    create game board

---
+++ components/initialization_board.py
@@ -0,0 +1,21 @@
+from typing import List ,Dict ,Any
+
+NUMBER_COLUMNS = 7
+NUMBER_LIENS = 6
+
+
+def create_game_board(number_liens: int= NUMBER_LIENS,
+        number_columns: int= NUMBER_COLUMNS) -> List[List[Dict[str,Any]]]:
+    game_board = []
+    for line in range(number_liens):
+        line = []
+        for column in range(number_columns):
+            line.append({'is_active':False,'type_player':None})
+        game_board.append(line)
+    return game_board
+
+
+
+
+
+
